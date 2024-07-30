5efa0d08493c15f6b34ca7f66102060288775fd5
ankithans <ankithans1947@gmail.com>
2024-07-31 00:02:46 +0530

    chore: add shadcn

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,11 @@
 import { signIn } from "@/app/api/auth/[...nextauth]/options";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
 
 const SignIn = () => {
-	return (
-		<form
-			action={async (formData) => {
-				"use server";
-				await signIn("credentials", formData);
-			}}
-		>
-			<label>
-				Email
-				<input name="email" type="email" />
-			</label>
-			<label>
-				Password
-				<input name="password" type="password" />
-			</label>
-			<button>Sign In</button>
-		</form>
-	);
+	return <div></div>;
 };
 
 export default SignIn;
